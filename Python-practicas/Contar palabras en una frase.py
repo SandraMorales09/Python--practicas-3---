99d95f7ea37446baf4031933892d4ef46d450a10
Python-practicas-3-- <114303948+SandraMorales09@users.noreply.github.com>
2025-05-23 23:12:58 -0600

    Delete Python-practicas directory

--- Python-practicas/Contar palabras en una frase.py
+++
@@ -1,7 +0,0 @@
-def contar_palabras(frase):
-    palabras = frase.split()
-    return len(palabras)
-
-# Ejemplo de uso
-frase = "Este es un ejemplo de Python"
-print(f"Cantidad de palabras: {contar_palabras(frase)}")  # Resultado: 6
--- Python-practicas/Convertir grados Celsius a Fahrenheit.py
+++
@@ -1,6 +0,0 @@
-def celsius_a_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
-
-# Ejemplo de uso
-celsius = 25
-print(f"{celsius}°C equivalen a {celsius_a_fahrenheit(celsius)}°F.")  # Resultado: 77°F
